e83ad822c9dd91b9c7a7d7a42471c56cc1fdc561
SanderBorgmans <sanderborgmans@gmail.com>
2019-08-13 14:26:45 +0200

    Fixed path

--- vasp/potentials/link_potentials.sh
+++ vasp/potentials/link_potentials.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ln -sf $VSC_DATA_VO/POTCAR_PBE ./pyiron/resources/vasp/potentials/potpaw_PBE
-ln -sf $VSC_DATA_VO/POTCAR_LDA ./pyiron/resources/vasp/potentials/potpaw 
+ln -sf $VSC_DATA_VO/POTCAR_PBE ./potpaw_PBE
+ln -sf $VSC_DATA_VO/POTCAR_LDA ./potpaw 

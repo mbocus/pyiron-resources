fdc3a56353392d0204736233af0e1c8e4341ec76
SanderBorgmans <sanderborgmans@gmail.com>
2020-11-17 11:15:00 +0100

    omit rm chk for at&mol classes

--- gaussian/bin/run_gaussian_2018_mpi.sh
+++ gaussian/bin/run_gaussian_2018_mpi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ml purge
 ml load Gaussian/g16_A.03-intel-2019a
-g16 input.com -p=$1 && formchk input.chk input.fchk && rm input.chk
+g16 input.com -p=$1 && formchk input.chk input.fchk

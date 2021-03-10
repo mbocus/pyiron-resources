4f96c8f3bb7de5c44973f5e169d33522ef6bf1c3
SanderBorgmans <sanderborgmans@gmail.com>
2021-03-10 11:14:52 +0100

    Fix gpu support

--- qchem/bin/run_qchem_2021_gpu_mpi.sh
+++ qchem/bin/run_qchem_2021_gpu_mpi.sh
@@ -2,5 +2,6 @@
 ml purge
 module use $VSC_DATA_VO/ebqc/modules
 module load Q-Chem/5.3.2-GPU
+export BRIANQC_INSTALL_PATH=/data/gent/vo/000/gvo00003/ebqc/software/Q-Chem/5.3.2-GPU/brianqc_qchem
 export QCSCRATCH=.
-qchem -nt $1 job.in job.out
+qchem -nt $1 -gpu job.in job.out

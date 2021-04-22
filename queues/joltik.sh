cef008bc28e57a5954042974fa43eb9a55b4665f
SanderBorgmans <sanderborgmans@gmail.com>
2021-04-22 16:18:48 +0200

    Error parsing for joltik

--- queues/joltik.sh
+++ queues/joltik.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+{% macro abort(error) %}
+    {{ None['[ERROR] ' ~ error][0] }}
+{% endmacro %}
 {%- set cpn = 32 %}
 {%- set gpn = 4 %}
 {%- set nodes = ((cores-1)//cpn) + 1 %}
-{% assert(gpus<gpn*nodes , 'Number of gpus is too large for the number of nodes you reserved.') %}
+{% if gpus>gpn*nodes %}
+{{ abort('Number of gpus is too large') }}
+{% endif %}
 {%- if  cores%nodes == 0 %}
 #SBATCH --nodes={{nodes}} --ntasks={{cores}} --ntasks-per-node={{cores//nodes}} --gres=gpu:{{gpus}} --cpus-per-gpu={{cores//gpus}}
 {%- else %}

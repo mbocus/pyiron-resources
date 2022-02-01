8894f4c22999eaee004f6d74355c714c8f8a341a
SanderBorgmans <sanderborgmans@gmail.com>
2022-02-01 16:09:01 +0100

    Separate table generation and execution for yaff/lammps jobs

--- yaff/bin/run_yaff_2019_lammps.sh
+++ yaff/bin/run_yaff_2019_lammps.sh
@@ -3,4 +3,6 @@ ml purge
 ml yaff/1.6.0-intel-2019b-Python-3.7.4
 ml LAMMPS/patch_20Nov2019-intel-2019b
 ml PLUMED/2.5.3-intel-2019b-Python-3.7.4
-python yscript.py > yaff.log
+
+python table_script.py > table.log
+mpirun -np 1 python yscript.py > yaff.log
